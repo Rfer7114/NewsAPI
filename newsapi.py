91aa3b943e0ddec4f4723d73c797ab42b4dbd50b
Rfer7114 <russel.workid@gmail.com>
2023-11-14 11:00:32 +0100

    added URL, added en language, range to 10 news items for tesla.

--- newsapi.py
+++ newsapi.py
@@ -2,17 +2,26 @@ import requests
 import send_email as se
 
 api_key = "0ae7df4094b845f3843167315ff482b1"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-10-14&sortBy=publishedAt&apiKey=0ae7df4094b845f3843167315ff482b1"
+url = "https://newsapi.org/v2/everything?"\
+       "q=tesla&from=2023-10-14&"\
+       "sortBy=publishedAt&"\
+       "apiKey=0ae7df4094b845f3843167315ff482b1&"\
+       "language=en"
 
 data = requests.get(url)
 contents = data.json()
 
 message = ""
-for article in contents["articles"]:
+for article in contents["articles"][:10]:
     title = article['title']
     description = article['description']
     if article['title'] is not None:
-        message = message + f"News Title: {title} \n Description: {description} \n\n"
+        message = (f"Subject: Today's News from NewsAPI "
+                   f"\n"
+                   f"{message} \n"
+                   f"News Title: {title} \n"
+                   f"Description: {description} \n\n"
+                   f"URL: {article['url']}")
 
 message = message.encode('utf-8')
 se.send_email(message)
